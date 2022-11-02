a4ebac21e6d54cc10a46e5799ac1bf15394741ff
Narayan Geed <geed.narayan9@gmail.com>
2022-11-02 18:57:11 +0530

    initial commit

---
+++ src/test/java/com/ngttech/demo/SpringBootJwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+/*
+ * package com.ngttech.demo;
+ * 
+ * 
+ * import org.junit.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class SpringBootJwtDemoApplicationTests {
+ * 
+ * @Test public void contextLoads() { }
+ * 
+ * }
+ */
